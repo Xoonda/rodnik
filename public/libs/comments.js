8bee028202fad633ff8fed91342b50499f7a2e87
Alex <qawajrush@yandex.ru>
2020-10-23 14:21:09 +0300

    firebase-test

---
+++ public/libs/comments.js
@@ -0,0 +1,19 @@
+// Set the configuration for your app
+  // TODO: Replace with your project's config object
+//   var config = {
+//     apiKey: "AIzaSyBCXxztKRi8q7pxUtftj7HxqOk4b64cizM",
+//     authDomain: "1:345539888666:web:dbcb4767cf21b01055b3a3.firebaseapp.com",
+//     databaseURL: "https://arzrodnik-27f4e.firebaseio.com",
+//     storageBucket: "jbjkhb.appspot.com"
+//   };
+//   firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
+  
+//   var userId = firebase.auth().currentUser.uid;
+//   console.log(userId);
+// return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+//   var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+  
+// });
